3429c900c51bfb88783eea27ab94049f9f3482a6
Eran KT <eran@ktfgalaxy.com>
2025-01-04 14:45:37 +0100

    added main page with selection buttons

---
+++ src/app/d3-charts/charts-models.ts
@@ -0,0 +1,11 @@
+export enum BarsTypesEnum {
+  bar = 'bar',
+  pie = 'pie',
+  scatter = 'scatter',
+}
+
+export const BarsTypesKeys = [
+  'bar',
+  'pie',
+  'scatter',
+];
--- src/app/d3-charts/d3-charts.component.ts
+++ src/app/d3-charts/d3-charts.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BarsTypesEnum, BarsTypesKeys } from './charts-models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-d3-charts',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './d3-charts.component.scss'
 })
 export class D3ChartsComponent {
+  BarsTypesKeys = BarsTypesKeys;
+  constructor(private router: Router) {
 
+  }
+
+  chartSelected(type: any) {
+    let routeUrl: string = '';
+    switch (type) {
+      case BarsTypesEnum.bar:
+        routeUrl = 'bar-chart/';
+        break;
+      case BarsTypesEnum.pie:
+        routeUrl = 'pie-chart/';
+        break;
+      case BarsTypesEnum.scatter:
+        routeUrl = 'scatter-chart/';
+        break;
+
+      default:
+        break;
+    }
+    this.router.navigateByUrl(routeUrl);
+
+  }
 }
