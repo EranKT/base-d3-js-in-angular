0ba617aac708286792bad7e34066267eeeca282a
Eran KT <eran@ktfgalaxy.com>
2025-01-04 14:11:55 +0100

    create charts module

---
+++ src/app/d3-charts/d3-charts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-d3-charts',
+  standalone: true,
+  imports: [],
+  templateUrl: './d3-charts.component.html',
+  styleUrl: './d3-charts.component.scss'
+})
+export class D3ChartsComponent {
+
+}
